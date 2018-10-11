b2dba95effb45df33dca73e0b56de7e7a5a644b1
Kristen Shea <kstar61@live.com>
2018-10-10 21:36:58 -0500

    *

---
+++ CG 14-2 (Calculator unit testing)/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_14_2__Calculator_unit_testing_
+{
+    public class Calculator
+    {
+
+        //properties for calculator class
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string EqAction { get; set; }
+
+
+        //separate equation string into elements to create instance of calculator class.
+        public Calculator(string equation)
+        {
+            //use try/catch to print an error message if equation was not entered in proper format.
+            //if it was typed correctly, split into Number1, Number2, and EqAction(equation action)
+            try
+            {
+                string[] equationSplit = equation.Split(" ");
+
+                for (int i = 0; i < equationSplit.Length; i++)
+                {
+                    if (i == 0)
+                        Number1 = int.Parse(equationSplit[i]);
+
+                    else if (i == 1)
+                        EqAction = equationSplit[i];
+
+                    else
+                        Number2 = int.Parse(equationSplit[i]);
+
+                }
+            }
+            //when user inputs equation incorrectly, it will tell them the equation is invalid.
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid equation.");
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        //method to complete calculation of equation.
+        public void Calculate()
+        {
+            double answer = 0;
+
+            if (EqAction == "+")
+                answer = Number1 + Number2;
+
+            else if (EqAction == "-")
+                answer = Number1 - Number2;
+
+            else if (EqAction == "*")
+                answer = Number1 * Number2;
+
+            else if (EqAction == "/")
+                answer = Number1 / Number2;
+
+            //don't need an else statement, exceptions are
+            //caught in the try/catch statement above.
+
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
--- CG 14-2 (Calculator unit testing)/Program.cs
+++ CG 14-2 (Calculator unit testing)/Program.cs
@@ -6,7 +6,25 @@ namespace CG_14_2__Calculator_unit_testing_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Update your calculator app with a unit testing project.Create tests that send 
+            //in valid input and tests that send in bad input.
+
+            //explain program, prompt user for equation, give example for correct format.
+            Console.WriteLine("Calculator to add, subtract, multiply, or divide two numbers. ");
+            Console.Write("Please type out equation to be calculated. (Example: 1 + 1) ");
+
+            //put equation into string
+            string equation = Console.ReadLine();
+
+            //create new instance of calculator class using equation string as input.
+            Calculator calculator = new Calculator(equation);
+
+            //initiate method from calculator class to calculate equation
+            calculator.Calculate();
+
+
+            Console.ReadLine();
+
         }
     }
 }

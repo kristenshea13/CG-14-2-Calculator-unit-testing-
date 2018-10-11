3d05380399eea1639b489a6ad46a9b00e173dd0a
Kristen Shea <kstar61@live.com>
2018-10-10 21:33:15 -0500

    Add project files.

---
+++ CG 14-2 (Calculator unit testing)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_14_2__Calculator_unit_testing_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

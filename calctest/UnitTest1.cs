cf23456c27a6f3f3d49ebaa4c8ac400a2dfbd5e3
Kristen Shea <kstar61@live.com>
2018-10-10 21:47:03 -0500

    *

---
+++ calctest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace calctest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            //[fact] for every method in test project
+            //should follow the three A's (AAA)
+            //arrange-set up data for test to run
+            //act-
+            //assert
+
+
+        }
+    }
+}
